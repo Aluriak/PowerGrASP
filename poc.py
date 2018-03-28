3448a07c5f9a6cb8dcc72d03a9c7a00129c89636
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-03-28 21:50:23 +0200

    main: more examples are tested (and correctly handled)

--- poc.py
+++ poc.py
@@ -5,7 +5,10 @@ from graph import Graph
 
 
 if __name__ == "__main__":
-    graphs = Graph.ccs_from_file('data/ddiam.lp')
+    # graphs = Graph.ccs_from_file('data/ddiam.lp')
+    # graphs = Graph.ccs_from_file('data/one_edge.lp')
+    # graphs = Graph.ccs_from_file('data/clique.lp')
+    graphs = Graph.ccs_from_file('data/perfectfit.lp')
     for idx, graph in enumerate(graphs, start=1):
 
         searchers = (CliqueSearcher(graph), BicliqueSearcher(graph))
@@ -20,13 +23,11 @@ if __name__ == "__main__":
                 if motif and (best_motif is None or motif.score > best_motif.score):
                     best_motif = motif
                     score_to_beat = best_motif.score
-            print('COMPRESS', best_motif.uid if best_motif else best_motif)
             if best_motif:
                 graph.compress(best_motif)
                 for searcher in searchers:
                     searcher.on_new_compressed_motif(best_motif)
             else:
                 break  # nothing to compress
-        print('END')
         graph.output('out/out_{}.bbl'.format(idx))
 
