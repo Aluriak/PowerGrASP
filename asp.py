cf876af7cc3a2dfd575a59f184803dbd3d840374
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-03-28 21:40:39 +0200

    use the constants to tune behavior

--- asp.py
+++ asp.py
@@ -1,12 +1,17 @@
 
 import clyngor
+from constants import COVERED_EDGES_FROM_ASP, SHOW_STORY
 
 
 def solve_motif_search(step:int, lowerbound:int, upperbound:int, files:iter, graph:str) -> iter:
     """Return atoms found in best model"""
     constants = {'k': step, 'lowerbound': lowerbound, 'upperbound': upperbound}
-    models = clyngor.solve(files=tuple(files), inline=str(graph), constants=constants)
-    print('SOLVE', models.command)
+    if COVERED_EDGES_FROM_ASP:
+        constants['covered_edges_from_asp'] = 1
+    models = clyngor.solve(files=tuple(files), inline=str(graph), constants=constants, stats=False)
+
+    if SHOW_STORY:
+        print('SOLVE', models.command)
 
     model = None
     for model in models.by_predicate.careful_parsing:
