5af38bd2f707d5c537dfc31afe7fe5f89fbf362d
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-03-29 10:38:03 +0200

    unit tests: not passing currently, test_cases needs work

---
+++ test/test_unambiguous_compression.py
@@ -0,0 +1,29 @@
+"""
+Test on graph compression.
+
+"""
+import os
+import pytest
+
+from main import compress_by_cc
+from .test_cases import cases
+
+
+
+def template_test_function(file:str, bubblelines:str):
+    def test_function():
+        found = unified_bubble(compress_by_cc(file))
+        expected = unified_bubble(bubblelines.splitlines(keepends=False))
+        assert found == expected
+    return test_function
+
+def unified_bubble(bubble_lines):
+    """Return the set of comparable lines found in given bubble lines"""
+    # filter out comments and blank lines
+    return set(line.strip() for line in bubble_lines
+               if not line.startswith('#') and len(line.strip()) > 0)
+
+
+for fname, bubble in cases.items():
+    func = template_test_function('data/' + fname, bubble)
+    globals()['test_' + os.path.basename(fname).replace('-', '_')] = func
