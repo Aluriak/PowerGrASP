bed135f60c51a47c8c3b34f62578779f6dd914eb
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-04-03 19:44:47 +0200

    finish packaging with setup, manifest and command line utility

--- powergrasp/__main__.py
+++ powergrasp/__main__.py
@@ -6,10 +6,13 @@ from . import cli
 from .routines import compress_by_cc
 
 
-if __name__ == "__main__":
+def run_cli():
     args = cli.parse_args(__doc__)
 
     with open(args.outfile, 'w') as fd:
         for line in compress_by_cc(args.infile):
             fd.write(line + '\n')
 
+
+if __name__ == "__main__":
+    run_cli()
