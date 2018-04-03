49a0a37eeb5c36a6fd7e2ad33bcd3092deb9f9da
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-04-03 16:37:53 +0200

    main: use MotifBatch + fix algo for multishot solving

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # import utils
 from searchers import CliqueSearcher, BicliqueSearcher
 from graph import Graph
+from constants import MULTISHOT_MOTIF_SEARCH, TEST_INTEGRITY
 
 
 def compress(graph:Graph) -> [str]:
@@ -15,8 +16,7 @@ def compress(graph:Graph) -> [str]:
         for searcher in sorted(searchers, key=lambda s: s.upperbound, reverse=True):
             motif = next(searcher.search(step, score_to_beat), None)
             if motif and (best_motif is None or motif.score > best_motif.score):
-                best_motif = motif
-                score_to_beat = best_motif.score
+                best_motif, score_to_beat = motif, motif.score
         if best_motif:
             graph.compress(best_motif)
             for searcher in searchers:
@@ -28,6 +28,7 @@ def compress(graph:Graph) -> [str]:
 
 def compress_multishot(graph:Graph) -> [str]:
     """Yield bubble lines found in graph"""
+    from motif_batch import MotifBatch
     searchers = (CliqueSearcher(graph), BicliqueSearcher(graph))
     step = 0
     while True:
@@ -35,14 +36,14 @@ def compress_multishot(graph:Graph) -> [str]:
         score_to_beat = 0
         best_motifs, best_motifs_score = None, 0
         for searcher in sorted(searchers, key=lambda s: s.upperbound, reverse=True):
-            score, motifs = searcher.search(step, score_to_beat)
-            if score and score > best_motifs_score:
-                best_motifs, best_motifs_score = motifs, score
+            motifs = MotifBatch(searcher.search(step, score_to_beat))
+            if motifs and motifs.score > best_motifs_score:
+                best_motifs, best_motifs_score = motifs, motifs.score
                 score_to_beat = best_motifs_score
         if best_motifs:
-            graph.compress_all(best_motifs)
+            step += graph.compress_all(best_motifs.non_overlapping_subset())
             for searcher in searchers:
-                searcher.on_new_compressed_motif(best_motif)
+                searcher.on_new_compressed_motif(best_motifs)
         else:
             break  # nothing to compress
     yield from graph.bubble_repr()
@@ -54,7 +55,10 @@ def compress_by_cc(fname:str) -> [str]:
     for idx, graph in enumerate(graphs, start=1):
         if idx > 1:  yield ''
         yield '# CONNECTED COMPONENT {}'.format(idx)
-        yield from compress(graph)
+        if MULTISHOT_MOTIF_SEARCH:
+            yield from compress_multishot(graph)
+        else:
+            yield from compress(graph)
 
 
 if __name__ == "__main__":
