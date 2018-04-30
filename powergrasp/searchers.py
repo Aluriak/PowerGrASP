6b6d75a647220658856bedcda3050181f14b884f
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-04-30 18:17:07 +0200

    improve bound computation for bicliques

--- powergrasp/searchers.py
+++ powergrasp/searchers.py
@@ -111,26 +111,35 @@ class BicliqueSearcher(MotifSearcher):
 
     name = 'biclique'
 
-    def compute_initial_lowerbound(self, graph:Graph) -> int:
-        """Maximal lowerbound is the score of biggest star, or the score of the biggest intersection"""
+    def compute_initial_bounds(self, graph:Graph) -> int:
+        """
+        Maximal lowerbound is the score of biggest star, or the score of the biggest intersection.
+        Minimal upperbound is the maximal possible association of the most connected nodes
+        """
         n = {node: frozenset(neighbors) for node, neighbors in graph.neighbors()}
+        sorted_degrees = sorted(tuple(map(len, n.values())), reverse=True)
+        upperbound = max(idx * deg for idx, deg in enumerate(sorted_degrees, start=1))
         biggest_star = max(len(neighbors) for _, neighbors in graph.neighbors())
         if BICLIQUE_LOWERBOUND_MAXNEI <= 1:
-            return biggest_star
+            lowerbound = biggest_star
         elif BICLIQUE_LOWERBOUND_MAXNEI == 2:
             maxnei2 = max(len(n[a] & n[b]) * 2 for a, b in itertools.combinations(n.keys(), r=2))
-            return max(maxnei2, biggest_star)
+            lowerbound = max(maxnei2, biggest_star)
         elif BICLIQUE_LOWERBOUND_MAXNEI >= 3:
-            maxnei = 0
+            lowerbound = 0
             for level in range(2, BICLIQUE_LOWERBOUND_MAXNEI + 1):
-                max_for_level = max(len(frozenset.intersection(*(n[s] for s in sets))) * level for sets in itertools.combinations(n.keys(), r=level))
-                if max_for_level <= maxnei: break
-                maxnei = max_for_level
-                if maxnei <= biggest_star: break
-            return max(maxnei, biggest_star)
+                try:
+                    max_for_level = max(len(frozenset.intersection(*(n[s] for s in sets))) * level for sets in itertools.combinations(n.keys(), r=level))
+                except ValueError:
+                    max_for_level = 0
+                if max_for_level < lowerbound: break
+                if lowerbound < biggest_star: break
+                lowerbound = max(lowerbound, max_for_level)
         else:
             maxnei2 = max(len(n[a] & n[b]) * 2 for a, b in itertools.combinations(n.keys(), r=2))
-            return max(maxnei2, biggest_star)
+            lowerbound = max(maxnei2, biggest_star)
+        return lowerbound, upperbound
+
 
     def _search(self, step:int, graph:Graph, lowerbound:int, upperbound:int) -> iter:
         graph = ''.join(graph.as_asp(step))
@@ -156,19 +165,27 @@ class NonStarBicliqueSearcher(MotifSearcher):
 
     name = 'non-star-biclique'
 
-    def compute_initial_lowerbound(self, graph:Graph) -> int:
-        """Maximal lowerbound is the score of biggest star, or the score of the biggest intersection"""
+    def compute_initial_bounds(self, graph:Graph) -> int:
+        """
+        Maximal lowerbound is the score of the biggest intersection.
+        Minimal upperbound is the maximal possible association of the most connected nodes
+        """
         n = {node: frozenset(neighbors) for node, neighbors in graph.neighbors()}
+        sorted_degrees = sorted(tuple(map(len, n.values())), reverse=True)
+        upperbound = max(idx * deg for idx, deg in enumerate(sorted_degrees, start=1))
         if BICLIQUE_LOWERBOUND_MAXNEI >= 3:
             maxnei = 0
             for level in range(2, BICLIQUE_LOWERBOUND_MAXNEI + 1):
-                max_for_level = max(len(frozenset.intersection(*(n[s] for s in sets))) * level for sets in itertools.combinations(n.keys(), r=level))
-                if max_for_level <= maxnei: break
+                try:
+                    max_for_level = max(len(frozenset.intersection(*(n[s] for s in sets))) * level for sets in itertools.combinations(n.keys(), r=level))
+                except ValueError:
+                    max_for_level = 0
+                if max_for_level < maxnei: break
                 maxnei = max_for_level
-            return maxnei
+            return maxnei, upperbound
         else:
             maxnei2 = max(len(n[a] & n[b]) * 2 for a, b in itertools.combinations(n.keys(), r=2))
-            return maxnei2
+            return maxnei2, upperbound
 
     def _search(self, step:int, graph:Graph, lowerbound:int, upperbound:int) -> iter:
         graph = ''.join(graph.as_asp(step))
